780884200c88d578e0c0a8b577d5a516cd86be5c
yangwansu <ywsaau77@gmail.com>
2021-06-12 11:44:39 +0900

    Polishing Add Test for ApplicationEvents

--- app/src/test/java/org/slipp/masil/games/infrastructures/events/HighLowPlayingContextDomainEventTest.java
+++ app/src/test/java/org/slipp/masil/games/infrastructures/events/HighLowPlayingContextDomainEventTest.java
@@ -1,13 +1,17 @@
 package org.slipp.masil.games.infrastructures.events;
 
-import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.Test;
 import org.slipp.masil.games.application.HighLowApplicationService;
-import org.slipp.masil.games.domains.highrow.HighLowPlayingContext;
 import org.slipp.masil.games.domains.highrow.HighLowPlayingContextRepository;
+import org.slipp.masil.games.domains.highrow.StartedHighLowPlay;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.event.ApplicationEvents;
+import org.springframework.test.context.event.RecordApplicationEvents;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RecordApplicationEvents
 @SpringBootTest
 class HighLowPlayingContextDomainEventTest {
 
@@ -17,11 +21,13 @@ class HighLowPlayingContextDomainEventTest {
     @Autowired
     HighLowPlayingContextRepository repository;
 
+    @Autowired
+    ApplicationEvents applicationEvents;
+
     @Test
-    void name() {
+    void publishStartedEvent() {
         highLowApplicationService.start();
 
-        HighLowPlayingContext c = repository.findById(0L);
-        System.out.println(c);
+        assertThat(applicationEvents.stream(StartedHighLowPlay.class)).hasSize(1);
     }
 }
