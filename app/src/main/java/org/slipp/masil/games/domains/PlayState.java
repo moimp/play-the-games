aaea41fc87e30aa5c5cc64532b157d0e27aae3d3
yangwansu <ywsaau77@gmail.com>
2021-06-18 21:36:09 +0900

    step3

--- app/src/main/java/org/slipp/masil/games/domains/PlayState.java
+++ app/src/main/java/org/slipp/masil/games/domains/PlayState.java
@@ -4,6 +4,29 @@ public enum PlayState {
     INIT, ON_GAME, ENDED;
 
     public PlayState changeTo(PlayState state) {
+        if(isOn() && state == ON_GAME) {
+            throw new IllegalStateException();
+        }
+
+        if(! isOn() && state == ENDED) {
+            throw new IllegalStateException();
+        }
+
+        if(isOff() && state == ON_GAME) {
+            throw new IllegalStateException();
+        }
+
+        if(isOff() && state == ENDED) {
+            throw new IllegalStateException();
+        }
         return state;
+    }
+
+    public boolean isOn() {
+        return ON_GAME == this;
+    }
+
+    public boolean isOff() {
+        return ENDED == this;
     }
 }
