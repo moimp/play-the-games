e157dfcdca9e906a15dd314258e7b9fd85c09c64
lenkim <joenggyu0@gmail.com>
2021-06-26 09:49:22 +0900

    게임라운지 페이지 부트스트랩 적용완료

--- app/src/main/java/org/slipp/masil/games/web/GamesController.java
+++ app/src/main/java/org/slipp/masil/games/web/GamesController.java
@@ -10,7 +10,7 @@ public class GamesController {
     @GetMapping(value = "/games")
     public ModelAndView games() {
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("gameLounge");
+        modelAndView.setViewName("index");
         modelAndView.addObject("gameId", "1");
 
         return modelAndView;
